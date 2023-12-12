2671ecff9e4c61519587712f2bbe80cf23f17c84
Juan Manuel Santamaria Arenas <juanmanuelsantamariaarenas01@gmail.com>
2023-12-11 21:14:08 -0500

    Practice Flask

---
+++ routes/contacts.py
@@ -0,0 +1,20 @@
+from flask import  Blueprint, render_template
+
+contacts = Blueprint("contacts", __name__)
+
+@contacts.route("/")
+def home():
+    return render_template("index.html")
+
+@contacts.route("/new")
+def add_contact():
+    return "Saving a contact"
+
+@contacts.route("/update")
+def update():
+    return "Update a contact"
+
+@contacts.route("/delete")
+def delete():
+    return "Delete a contact"
+
